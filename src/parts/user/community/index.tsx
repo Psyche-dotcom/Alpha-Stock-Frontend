bafbc14263bda5de4ec0506619e0fb091186b312
Saheed Olawale Babatunde <53710927+Psyche-dotcom@users.noreply.github.com>
2025-05-08 03:22:04 +0100

    update me

--- src/parts/user/community/index.tsx
+++ src/parts/user/community/index.tsx
@@ -114,8 +114,8 @@ const Community = () => {
   // 👇 Setup SignalR connection once
   useEffect(() => {
     const newConnection = new signalR.HubConnectionBuilder()
-      .withUrl("https://localhost:7013" + "/chatHub")
-      // .withUrl(process.env.NEXT_PUBLIC_API_URL + "/chatHub")
+      .withUrl(process.env.NEXT_PUBLIC_API_URL + "/chatHub")
+      //.withUrl("https://localhost:7013" + "/chatHub")
       .withAutomaticReconnect()
       .build();
 

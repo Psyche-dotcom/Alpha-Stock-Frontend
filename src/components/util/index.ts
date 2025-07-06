80dc356ddb31ebb49752de2bcf51042f66d95253
Saheed Olawale Babatunde <53710927+Psyche-dotcom@users.noreply.github.com>
2025-07-06 22:44:32 +0100

    fix all little issued

--- src/components/util/index.ts
+++ src/components/util/index.ts
@@ -1912,11 +1912,11 @@ export function formatMoneyNumber2(
   const sign = num < 0 ? "-" : "";
 
   if (absNum >= 1_000_000_000_000) {
-    return sign + (absNum / 1_000_000_000_000).toFixed(2) + "T";
+    return "$" + sign + (absNum / 1_000_000_000_000).toFixed(2) + "T";
   } else if (absNum >= 1_000_000_000) {
-    return sign + (absNum / 1_000_000_000).toFixed(2) + "B";
+    return "$" + sign + (absNum / 1_000_000_000).toFixed(2) + "B";
   } else if (absNum >= 1_000_000) {
-    return sign + (absNum / 1_000_000).toFixed(2) + "M";
+    return "$" + sign + (absNum / 1_000_000).toFixed(2) + "M";
   } else if (absNum >= 1_000) {
     return sign + (absNum / 1_000).toFixed(2) + "K";
   } else {
@@ -1944,7 +1944,9 @@ export function formatDateToHumanReadable(
   return date.toLocaleDateString(locale, options);
 }
 
-export function formatDateToHumanReadableNew(isoDateString: string | undefined | null): string {
+export function formatDateToHumanReadableNew(
+  isoDateString: string | undefined | null
+): string {
   // Check if isoDateString is null, undefined, or an empty string
   if (!isoDateString) {
     return "-";
